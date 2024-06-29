4254acb2cd89ab4ffeb016a497ec8ceb086ec06f
Lucho <lucho_l.f@hotmail.com>
2024-06-29 10:15:03 -0300

    Add components Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react";
+
 const TURNS = {
   X: "X",
   O: "O",
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const clasName = `square ${isSelected ? 'is-selected' : ''}`
+
+  return <div className={clasName}>{children}</div>;
 };
 
-const board = Array(9).fill(null);
-
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            index
+            <Square 
+              key={index}
+              index={index}
+            >
+              
+            </Square>  
         )
         })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   );
